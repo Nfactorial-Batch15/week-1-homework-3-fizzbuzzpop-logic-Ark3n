44f8278bdd588bd6a10ed01ca3b7c2ab924a0496
Arken <Arken.Sarsenov@outlook.com>
2021-12-05 23:22:51 +0500

    Initial Commit

--- FizzBuzzPopLogic/main.swift
+++ FizzBuzzPopLogic/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+var numbers = 1...100
+numbers.forEach {number in checkNumber(number)}
 
+func checkNumber(_ number: Int){
+    if number % 15 == 0 {
+        print("fizzy Buzz")
+    }else if number % 21 == 0 {
+        print("fizz pop")
+    }else if number % 3 == 0 {
+        print("fizzy")
+    }else if number % 5 == 0 {
+        print("buzz")
+    }else if number % 7 == 0 {
+        print("pop")
+    }else {
+        print(number)
+  }
+}
